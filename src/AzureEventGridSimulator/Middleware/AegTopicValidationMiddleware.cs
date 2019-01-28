f1f617fc4ce4dc0a15ded952b37e74f9be993034
Paul Mcilreavy <paul.mcilreavy@readify.net>
2019-01-29 08:50:06 +1000

    Default topic to something.

--- src/AzureEventGridSimulator/Middleware/AegTopicValidationMiddleware.cs
+++ src/AzureEventGridSimulator/Middleware/AegTopicValidationMiddleware.cs
@@ -43,7 +43,7 @@ namespace AzureEventGridSimulator.Middleware
                     }
                 }
 
-                eventGridEvent.Topic = expectedTopicPath;
+                eventGridEvent.Topic = expectedTopicPath ?? $"/azure/event/grid/simulator/{topicSettings.Name}";
                 eventGridEvent.MetadataVersion = "1";
             }
 
