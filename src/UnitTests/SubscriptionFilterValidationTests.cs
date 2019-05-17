1812a1ae62bbda986676725f89fe5a3cda9ce785
Henry Carter <henry.carter@m-kopa.com>
2019-05-17 10:13:11 +0100

    Added unit test project

---
+++ src/UnitTests/SubscriptionFilterValidationTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class SubscriptionFilterValidationTests
+    {
+        [Test]
+        public void TestMaxAdvancedFilterValidation()
+        {
+            Assert.Pass();
+        }
+    }
+}
