24036759906e36537602e219d64aa4637164f34f
James Foster <james.foster@existec.com>
2019-09-30 10:30:32 +0100

    Allow specifying the config file on the command line

--- src/UnitTests/ConfigurationLoadingTests.cs
+++ src/UnitTests/ConfigurationLoadingTests.cs
@@ -11,12 +11,50 @@ namespace UnitTests
         [Fact]
         public void TestConfigurationLoad()
         {
-            var json = "{\"topics\":[{\"name\":\"MyAwesomeTopic\",\"port\":60101,\"key\":\"TheLocal+DevelopmentKey=\",\"subscribers\":[{\"name\":\"LocalAzureFunctionSubscription\",\"endpoint\":\"http://localhost:7071/runtime/webhooks/EventGrid?functionName=PersistEventToDb\",\"filter\":{\"includedEventTypes\":[\"some.special.event.type\"],\"subjectBeginsWith\":\"MySubject\",\"subjectEndsWith\":\"_success\",\"isSubjectCaseSensitive\":true,\"advancedFilters\":[{\"operatorType\":\"NumberGreaterThanOrEquals\",\"key\":\"Data.Key1\",\"value\":5},{\"operatorType\":\"StringContains\",\"key\":\"Subject\",\"values\":[\"container1\",\"container2\"]}]}}]},{\"name\":\"ATopicWithNoSubscribers\",\"port\":60102,\"key\":\"TheLocal+DevelopmentKey=\",\"subscribers\":[]}]}";
+            var json = @"
+{
+    ""topics"": [{
+        ""name"": ""MyAwesomeTopic"",
+        ""port"": 60101,
+        ""key"": ""TheLocal+DevelopmentKey="",
+        ""subscribers"": [{
+            ""name"": ""LocalAzureFunctionSubscription"",
+            ""endpoint"":""http://localhost:7071/runtime/webhooks/EventGrid?functionName=PersistEventToDb"",
+            ""filter"": {
+                ""includedEventTypes"":[""some.special.event.type""],
+                ""subjectBeginsWith"":""MySubject"",
+                ""subjectEndsWith"":""_success"",
+                ""isSubjectCaseSensitive"":true,
+                ""advancedFilters"": [{
+                    ""operatorType"":""NumberGreaterThanOrEquals"",
+                    ""key"":""Data.Key1"",
+                    ""value"":5
+                },
+                {
+                    ""operatorType"":""StringContains"",
+                    ""key"":""Subject"",
+                    ""values"":[""container1"",""container2""
+                ]}
+            ]}
+        }]
+    },
+    {
+        ""name"":""ATopicWithNoSubscribers"",
+        ""port"":60102,
+        ""key"":""TheLocal+DevelopmentKey="",
+        ""subscribers"":[]
+    }]
+}";
 
             var settings = JsonConvert.DeserializeObject<SimulatorSettings>(json);
+
             settings.ShouldNotBeNull();
             settings.Topics.ShouldNotBeNull();
-            settings.Topics.ShouldAllBe(t => t.Subscribers.All(s => s.Filter != null) && t.Subscribers.All(s => s.Filter.AdvancedFilters != null));
+            settings.Topics.ShouldAllBe(t =>
+                t.Subscribers.All(s => s.Filter != null) &&
+                t.Subscribers.All(s => s.Filter.AdvancedFilters != null)
+            );
+
             Should.NotThrow(() => { settings.Validate(); });
         }
     }
