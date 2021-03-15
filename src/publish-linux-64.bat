d7af8f14d7d9b26cf3ecee011810a55317b16899
Paul Mcilreavy <paul@mcilreavy.com>
2021-03-15 22:23:56 +1000

    Put shared publish settings in csproj

--- src/publish-linux-64.bat
+++ src/publish-linux-64.bat
@@ -8,7 +8,7 @@ ECHO REMOVE OUTPUT FOLDER "build-linux-64"
 rd /S /Q "build-linux-64"
 
 ECHO PUBLISHING SOLUTION
-dotnet publish ./AzureEventGridSimulator/AzureEventGridSimulator.csproj -o "../build-linux-64" -v q -c Release --self-contained true -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishSingleFile=true -p:PublishTrimmed=true -f net5.0 -r linux-x64 /nologo
+dotnet publish ./AzureEventGridSimulator/AzureEventGridSimulator.csproj -o "../build-linux-64" -c Release -r linux-x64
 
 ECHO DONE!
 pause
