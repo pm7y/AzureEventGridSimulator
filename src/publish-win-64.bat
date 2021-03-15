6e9894c2c86c736da83dec412c46644f17b963e3
Paul Mcilreavy <paul@mcilreavy.com>
2021-03-15 22:01:50 +1000

    Publish tweaks

--- src/publish-win-64.bat
+++ src/publish-win-64.bat
@@ -8,7 +8,7 @@ ECHO REMOVE OUTPUT FOLDER "build-win-64"
 rd /S /Q "build-win-64"
 
 ECHO PUBLISHING SOLUTION
-dotnet publish ./AzureEventGridSimulator/AzureEventGridSimulator.csproj -o "../build-win-64" -v q -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -f netcoreapp3.1 -r win-x64 /nologo
+dotnet publish ./AzureEventGridSimulator/AzureEventGridSimulator.csproj -o "../build-win-64" -v q -c Release --self-contained true -p:PublishReadyToRun=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimUnusedDependencies=true -f net5.0 -r win-x64 /nologo
 
 ECHO DONE!
 pause
