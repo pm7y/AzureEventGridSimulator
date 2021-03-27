d450e1a83312e35cbcb9b5ff4f7ad2600062ee73
Paul Mcilreavy <paul@mcilreavy.com>
2021-03-27 10:52:59 +1000

    Add docker file and docker-compose file

---
+++ DockerComposeUp.ps1
@@ -0,0 +1,11 @@
+﻿# Note: Powershell may throw misleading 'error' message
+#       https://stackoverflow.com/questions/2095088/error-when-calling-3rd-party-executable-from-powershell-when-using-an-ide
+
+
+# rebuild the image and run it using the settings from 'docker-compose.yml'
+docker-compose up   --build `
+                    --force-recreate `
+                    --always-recreate-deps `
+                    --remove-orphans `
+                    --no-log-prefix `
+                    --detach
