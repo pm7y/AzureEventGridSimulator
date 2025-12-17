3014072129477551c134298b4dbd087b89175d80
Paul Mcilreavy <3075792+pm7y@users.noreply.github.com>
2025-12-17 22:22:00 +1000

    chore: Improve docker ps scripts (#222)

--- docker/docker-compose-up.ps1
+++ docker/docker-compose-up.ps1
@@ -1,6 +1,9 @@
 ﻿# (re)build the image and run it using the settings from 'docker-compose.yml'
 
-docker-compose up   --build `
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+docker-compose -f "$scriptDir/docker-compose.yml" up `
+                    --build `
                     --force-recreate `
                     --remove-orphans `
                     --detach
